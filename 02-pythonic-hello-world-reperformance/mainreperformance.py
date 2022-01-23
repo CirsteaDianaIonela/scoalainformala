7120db0423bc6cf15849ac2a5d36e368a99562c0
unknown <cirsteagdiana@gmail.com>
2022-01-23 17:24:10 +0200

    un mesaj3

---
+++ 02-pythonic-hello-world-reperformance/mainreperformance.py
@@ -0,0 +1,55 @@
+# print("Numele meu este Diana")
+
+# print(1, type(1)) - int numere intregi
+# print(7.5, type(7.5)) - float numer cu zecimale
+# print (3j, type(3j)) - complex
+# print(int(1.1)) - convertire din float in int
+# print(float(1)) - convertire din int in float
+
+# print(1+1) adunare
+# print(1-1) scadere
+# print(1*0) - inmultire
+# print(8/2) imparire
+# print(5%2) - restul
+# print(5//2) - catul
+# print(5**3) #ridicare la putere
+
+# print(5==2) - operator de egalitate
+# print(5!=2) - operator de neegalitate
+# print(5>2) #- operator mai mare
+# print(5<2) #- operator mai mic
+# print(5<=2) #- operator mai mic sau egal
+# print(5>=2) #- operator mai mare sau egal
+
+# print(5>2 and 3>1) = operator logic AND
+# print(1>2 or 3>5) operator logic OR
+# print(not(5>2 and 3>1)) #operator logic negare NOT
+
+# print(7 is 7) operatori de identitate IS
+# print(8 is not 7) #operatori de identitate IS NOT
+
+# print(1 in [1,2]) - operatori de apartenenta IN
+# print(1 not in [1,2]) #- operatori de apartenenta IN
+
+#variabile
+# myvar1, myvar2 = 0,2 #declararea mai multor variabile in acelasi timp
+# print(myvar1, myvar2)
+# print(id(myvar1)) -ID
+# myvar1 +=2 #asignare prin inmultire
+# myvar2 = myvar2**3 #asignare prin ridicare la putere
+# print(myvar1,myvar2)
+
+#string = siruri de caractere
+# print("Ana are mere")
+# variabila = "Ana 'are' mere"
+# print(variabila)
+# variabila = "Ana \"are\" mere"
+# print(variabila)
+variabila = "Ana are mere si \nAna are cozonaci"
+print(variabila)
+
+
+
+
+
+
