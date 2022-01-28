095890c00dbdfa5419dfa8a8ecab67aacea02767
unknown <cirsteagdiana@gmail.com>
2022-01-28 14:49:46 +0200

    mesaj

---
+++ 03-data-structure/exercitii_optionale.py
@@ -0,0 +1,7 @@
+#Scrieti un program ce va numara cate caractere are un sir de caractere dat de utilizator.
+#Aceasta numarare sa se realizeze cu ajutorul unui for fara a utiliza len(). La final afisati rezultatul
+
+
+
+
+
