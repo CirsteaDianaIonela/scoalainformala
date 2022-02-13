7780797acdfaa27a158bf3c1611d5e320a6745f9
unknown <cirsteagdiana@gmail.com>
2022-02-13 19:04:52 +0200

    Validator CNP

--- teme/tema2.py
+++ teme/tema2.py
@@ -1,5 +1,5 @@
 #● Să se scrie o funcție care citește de la tastatură și returnează valoarea dacă aceasta este un număr întreg, altfel returnează valoarea 0.
-
+#
 def functie():
     msg = " "
     nr_introdus=input("Spune-mi o valoare: ")
@@ -11,6 +11,28 @@ def functie():
     return msg
 msg = functie()
 print(msg)
+
+
+# Să se scrie o funcție care primește un număr nedefinit de parametrii și să se calculeze suma parametrilor care reprezintă numere întregi sau reale.
+# ○ your_function(1, 5, -3, ‘abc’, [12, 56, ‘cad’]) va returna 3 (1 + 5 - 3).
+#
+# ○ your_function() va returna 0.
+#
+# ○ your_function(2, 4, ‘abc’, param_1=2) va returna 6 (2 + 4)
+#
+# def functie_2 (1, 5, -3, ‘abc’, [12, 56, ‘cad’]):
+
+
+def your_function(*lista):
+    if len(list(lista)) == 0:
+        print("0")
+    else:
+        print(sum([d for d in lista if isinstance(d, int)]))
+
+your_function()
+your_function(1, 5, -3, "abc", [12, 56, "ca"])
+your_function(2, 4, "ab", "param_1 = 2")
+
 
 
 
