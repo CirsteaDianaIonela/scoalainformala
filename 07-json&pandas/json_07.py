9e0d97a9dc20ab11e15967b96b54188bdd24cf14
unknown <cirsteagdiana@gmail.com>
2022-02-23 21:37:27 +0200

    pandas

---
+++ 07-json&pandas/json_07.py
@@ -0,0 +1,26 @@
+#extrage si stoca date
+import json
+x = '{"name": "Ion", "age": 30, "city": "Iasi"}'
+y = json.loads(x)
+print(y, type (y))
+
+z=y
+print(z, json.dumps(z), type(z))
+a=json.dumps(["mere","pere"])
+print(a, json.dumps(a), type(a))
+a = "hello"
+print(a, json.dumps(a), type(a))
+a = 41
+print(a, json.dumps(a), type(a))
+a = 31.32
+print(a, json.dumps(a), type(a))
+a = True
+print(a, json.dumps(a), type(a))
+a = None
+print(a, json.dumps(a), type(a))
+
+
+
+
+
+
